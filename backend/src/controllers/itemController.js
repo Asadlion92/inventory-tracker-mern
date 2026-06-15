b6eebe6a17927db4dcd74fcf187998e36298af59
Jafar Asad <rafajdasa@gmail.com>
2026-06-15 06:21:20 -0400

    completed the CRUD for backend

--- backend/src/controllers/itemController.js
+++ backend/src/controllers/itemController.js
@@ -26,10 +26,29 @@ export const createItem = async (req, res) => {
     }
 };
 
-export const updateItem = () => {
-    res.status(200).json({message: "Item updated successfully!"})
+export const updateItem = async (req, res) => {
+    try {
+        const {name, quantity, price, category} = req.body;
+        const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        if (!updatedItem) return res.status(404).json({message: "Item not found"});
+        res.status(200).json({message: "Item updated successfully!"})
+    } catch (error) {
+        console.error("Error in updateItem controller", error);
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
 };
 
-export const deleteItem = () => {
-    res.status(200).json({message: "Item deleted successfully!"})
+export const deleteItem = async (req, res) => {
+    try {
+        const deletedItem = await Item.findByIdAndDelete(req.params.id);
+        if (!deletedItem) return res.status(404).json({message: "Item not found"});
+        res.status(400).json({message: "Item deleted successfully!"})
+    } catch (error) {
+        console.error("Error in deleteItem controller", error);
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
 };
