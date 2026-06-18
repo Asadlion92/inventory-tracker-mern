465a6374fa2e4fce205bf09377a461c8cedf58ce
Jafar Asad <rafajdasa@gmail.com>
2026-06-18 07:11:18 -0400

    completed backend and setting up ratelimiter and middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,28 @@ import express from "express";
 import itemRoutes from './routes/itemRoutes.js';
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/Ratelimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-connectDB();
-
 //middleware
 app.use(express.json());
+app.use(rateLimiter);
+
+// app.use((req, res, next) => {
+//     console.log("We just got a new request");
+//     next();
+// })
 
 app.use("/api/inventory", itemRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server is running on PORT:", PORT)
-});
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on PORT:", PORT)
+    });
+})
 
 export default app;
