a7bfb638300cc63d0e5d110f47cea240bd02d846
Jafar Asad <rafajdasa@gmail.com>
2026-06-27 08:55:11 -0400

    Was able to get the sidebar to work from daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import HomePage from './pages/HomePage';
 import AddItemPage from './pages/AddItemPage';
 import EditItemPage from './pages/EditItemPage';
 import toast from 'react-hot-toast';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
-    <div>
-    <button onClick={() => toast.error('wrong')}>Click Me</button>
+    <div data-theme='night'>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/add' element={<AddItemPage />} />
-        <Route path='/edit/:id' element={<EditItemPage />} />
+        <Route path='/' element={<Sidebar />}>
+          <Route index element={<HomePage />} />
+          <Route path='/add' element={<AddItemPage />} />
+          <Route path='/edit/:id' element={<EditItemPage />} />
+        </Route>
       </Routes>
     </div>
   )
