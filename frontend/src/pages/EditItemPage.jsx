247b37529ce8f1ba4a95c3e981bef20d81a2902f
Jafar Asad <rafajdasa@gmail.com>
2026-07-13 05:57:57 -0400

    EditItemPage complete

--- frontend/src/pages/EditItemPage.jsx
+++ frontend/src/pages/EditItemPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router';
-import { ArrowLeft, LoaderIcon, Trash2 } from 'lucide-react';
+import { ArrowLeft, LoaderIcon, Trash2, DollarSign } from 'lucide-react';
 import api from './../lib/axios';
 import { toast } from 'react-hot-toast';
 
@@ -11,6 +11,7 @@ const EditItemPage = () => {
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [loading, setLoading] = useState(true);
+  const [updating, setUpdating] = useState(false);
 
   const navigate = useNavigate();
   const {id} = useParams();
@@ -35,7 +36,34 @@ const EditItemPage = () => {
     fetchItem();
   }, [id]);
 
-  const handleDelete = () => {};
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    if (!itemName.trim() || quantity <=0 || price <= 0 || !category) {
+      toast.error("All fields are required");
+      return;
+    }
+
+      const updateItem = {
+        name: itemName,
+        quantity: Number(quantity),
+        price: Number(price),
+        category,
+      };
+
+    setUpdating(true);
+
+    try {
+      await api.put(`/inventory/${id}`, updateItem);
+      toast.success("Item updated successfully!");
+      navigate('/items');
+    } catch (error) {
+        console.log("Error updating item", error);
+        toast.error("Failed to update item");
+    } finally {
+      setUpdating(false);
+    }
+  };
 
   if (loading) {
     return (
@@ -52,10 +80,88 @@ const EditItemPage = () => {
           <ArrowLeft />
           Back To Items
         </Link>
-        <button onClick={handleDelete} className='btn btn-error btn-outline'>
-          <Trash2 className='h-5 w-5' />
-        </button>
       </div>
+
+      <div className='card bg-base-300 w-full'>
+        <div className='card-body'>
+          <h2 className='card-title text-2xl mb-4'>Edit Item</h2>
+          <form onSubmit={handleUpdate}>
+            <div className='form-control mb-4'>
+              <label className='label'>
+                <span className='label-text'>Item Name</span>
+              </label>
+              <input 
+                type="text"
+                placeholder='Enter item name'
+                className='input input-bordered'
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)}
+                />
+            </div>
+
+            <div className='form-control mb-4'>
+              <label className='label'>
+                <span className='label-text'>Quantity</span>
+              </label>
+              <input 
+                type="number"
+                min="1"
+                placeholder='Enter quantity'
+                className='input input-bordered'
+                value={quantity}
+                onChange={(e) => setQuantity(e.target.value)}
+                />
+            </div>
+
+            <div className='form-control mb-4'>
+              <label className='label'>
+                <span className='label-text'>Price</span>
+              </label>
+              <div className='join w-full'>
+                <input 
+                type="number"
+                min="0.01"
+                step="0.01"
+                placeholder='Enter price'
+                className='input input-bordered join-item w-full'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                />
+                <span className='join-item btn btn-disabled'><DollarSign /></span>
+              </div>
+
+            </div>
+
+            <div className='form-control mb-4'>
+              <label className='label'>
+                <span className='label-text'>Category</span>
+              </label>
+              <select
+                required
+                className='select select-bordered w-full' 
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">Select category</option>
+                <option value="Electronics">Electronics</option>
+                <option value="Tools">Tools</option>
+                <option value="Office Supplies">Office Supplies</option>
+                <option value="Safety Equipment">Safety Equipment</option>
+                <option value="Consumables">Consumables</option>
+                <option value="Others">Others</option>
+              </select>
+            </div>
+
+            <div className='card-actions justify-end'>
+              <button type='submit' className='btn btn-primary' disabled={updating} onClick={handleUpdate}>
+                {updating ? "Updating..." : "Update Item"}
+              </button>
+            </div>
+
+          </form>
+        </div>
+      </div>
+
     </div>
   )
 }
