9b099af5c3fd9d1fb8a6f71a1724219a71911a93
Jafar Asad <rafajdasa@gmail.com>
2026-07-25 07:20:17 -0400

    90% done with categories

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 
-const CategoryCard = ({category, totalItems, totalPrice}) => {
+const CategoryCard = ({category, totalItems, totalPrice, icon}) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
-      {/* <figure className="px-10 pt-10">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-          className="rounded-xl" />
-      </figure> */}
+    <div className="card w-72 shadow-xl">
       <div className="card-body items-center text-center">
+        {icon}
         <h2 className="card-title">{category}</h2>
         <p>{totalItems} Items</p>
         <p>${totalPrice.toFixed(2)}</p>
--- frontend/src/pages/Categories.jsx
+++ frontend/src/pages/Categories.jsx
@@ -1,9 +1,42 @@
 import CategoryCard from "../components/CategoryCard";
 import api from "../lib/axios";
 import { useEffect, useState } from "react";
+import { Laptop, Wrench, NotebookText, Shield, Package2, Ellipsis } from 'lucide-react'
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
+
+const categoryIcons = {
+  Consumables: {
+    icon: Package2,
+    color: "text-blue-500",
+  },
+
+  Electronics: {
+    icon: Laptop,
+    color: "text-purple-500",
+  },
+
+  "Office Supplies": {
+    icon: NotebookText,
+    color: "text-green-500",
+  },
+
+  Others: {
+    icon: Ellipsis,
+    color: "text-gray-500",
+  },
+
+  "Safety Equipment": {
+    icon: Shield,
+    color: "text-red-500",
+  },
+
+  Tools: {
+    icon: Wrench,
+    color: "text-orange-500",
+  },
+};
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -45,15 +78,23 @@ const Categories = () => {
         Categories
       </h1>
 
-      <div>
-        {categories.map((category) => (
-          <CategoryCard
-            key={category.category}
-            category={category.category}
-            totalItems={category.totalItems}
-            totalPrice={category.totalPrice}
-          />
-        ))}
+      <div className="flex justify-center align-center flex-wrap">
+
+        {categories.map((category) => {
+          const categoryInfo = categoryIcons[category.category]
+
+          const Icon = categoryInfo.icon;
+
+          return (
+            <CategoryCard
+              key={category.category}
+              category={category.category}
+              totalItems={category.totalItems}
+              totalPrice={category.totalPrice}
+              icon={<Icon className={categoryInfo.color} size={48} />}
+            />
+          );
+        })}
       </div>
     </div>
   );
